cfbc04515530661851b14634737946a4ab556324
sahil-nandal <79526843+Sahil-Nandal@users.noreply.github.com>
2024-12-10 10:41:57 +0000

    getting task by ID

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,9 +7,16 @@ import { CreateTaskDto } from './dto/create-task.dto';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  // localhost:3000/tasks
   @Get()
   gAT(): Task[] {
     return this.tasksService.getAllTasks();
+  }
+
+  // localhost:3000/tasks/bhdvudiychvcv
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
   }
 
   @Post()
