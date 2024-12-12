b5ed2a506343972d8ec3d2d0f911ad1bb8cf26e3
sahil-nandal <79526843+Sahil-Nandal@users.noreply.github.com>
2024-12-12 10:29:24 +0000

    Validation for deleting a taks which does not exist

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -62,7 +62,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id);
+    const found = this.tasks.find(task => task.id === id);
+    this.tasks = this.tasks.filter(task => task.id !== found.id);
   }
 
   updateTaskStatus(id: string, status: TaskStatus) {
